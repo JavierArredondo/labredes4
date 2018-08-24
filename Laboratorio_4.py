b0f7d5cc2cb8d67a2cd76ce88107fccd111f20ec
JavierArredondo <javier.arredondo.c@usach.cl>
2018-08-24 00:26:40 -0300

    Modulación digital

--- Laboratorio_4.py
+++ Laboratorio_4.py
@@ -46,17 +46,23 @@ def openWav(name):
         Ts = n / rate
         times = np.linspace(0, Ts, n)
         return (rate, data, times)
-
+"""
+Transformación de entero a binario, con n-bits
+"""
 def intToBin(num, bits):
         numBin = format(num, "b").zfill(bits)
         if(numBin[0] == "-"):
                 return "1"+numBin[1:]
         return numBin
-
+"""
+Calculo de la cantidad de bits máximos que se deben ocupar
+"""
 def sizeBin(maxi):
         return len(format(maxi, "b")) + 1
-        
 
+"""
+Transformacion de un arreglo de enteros a binario
+"""
 def arrayToBin(signal):
 	dataBin = []
 	bits = sizeBin(max(signal))
@@ -64,27 +70,61 @@ def arrayToBin(signal):
                 _bin = intToBin(data, bits)
                 for i in _bin:
                         dataBin.append(int(i))
-        return dataBin
+	return dataBin
 
-def ASKmodulation(signal):
-        A = 5
-        B = 2
-
-        timeASK = linspace(0, 1, 100)
-        amplASK = cos(2 * pi * timeASK)
-
-        ASK = []
-        
-        signalBin = arrayToBin(signal)
-        for i in  signalBin:
-                if(i == 1):
-                        ASK.append(A * amplASK)
-                else:
-                        ASK.append(B * amplASK)                    
-                        
+def graphDigitalData(binSignal, duration):
+        binFixed = []
+        for i in binSignal:
+                for j in range(0, 10):
+                        binFixed.append(i)
+        print(len(binFixed))
+        t = linspace(0, duration, len(binFixed))
+        print(len(t))
+        plt.plot(t[:1000], binFixed[:1000])
+        plt.ylim(0, 2)
+        plt.xlabel('Tiempo');
+        plt.ylabel('Amplitud');
+        plt.title('Señal Digital');
+        plt.grid(True)
+        plt.show()
         return
 
+"""
+Modulacion ASK
+"""
+def ASKmodulation(signal, bps, duration):
+        A = 4  # Amplitudes de cada coseno
+        B = 2
+        f = 2  # Frecuencia de carrier
+        timeCarrier = linspace(0, 1 - 1/bps, bps)   # Tiempo de un coseno, 1 seg, 100 muestras
+        dataCarrier = cos(2 * pi * f * timeASK)     # Carrier: onda coseno
+        
+        signalBin = arrayToBin(signal) # Obtenemos un arreglo de los datos en binario de la señal original
+        sizeBin = len(signalBin)       # Hay 1169808 elementos. Cantidad de bits totales en el audio.
+        # Graficamos la señal digital
+        graphDigitalData(signalBin, duration)
+        
+        cutBin = signalBin[0:10000] # Recortamos la muestra. Tomamos los 10000 primeros
+        ASK = []
+        for i in  cutBin:
+                if(i == 1):
+                        ASK = np.concatenate([ASK, (A * amplASK)])
+                else:
+                        ASK = np.concatenate([ASK, (B * amplASK)])
+        t = linspace(0, duration, sizeBin * len(amplASK)) # Creamos un vector tiempo para graficar. Dura 9s con un total de muchos puntos.
+        # Graficamos la señal modulada ASK
+        plt.plot(t[0:10000], ASK[0:10000])
+        plt.ylim(-6, 6)
+        plt.xlabel('Tiempo')
+        plt.ylabel('Amplitud')
+        plt.title('Señal ASK')
+        plt.grid(True)
+        plt.show()
+        return signalBin
 
-print("Start")
-print(intToBin(9, 10))
-print(intToBin(-9, 10))   
+
+print("Comienzo")
+rate, data, times = openWav("handel.wav")
+totalTime = len(data)/rate
+t = linspace(0, totalTime, len(data))
+ask = ASKmodulation(data, 100, totalTime)
